1eba0b2d5d21ab080133ebd1e91df275e9962954
Adam Wiggins <adam@heroku.com>
2010-12-10 12:48:39 -0800

    logging in ToC, make its beta status clear

--- toc.rb
+++ toc.rb
@@ -39,6 +39,7 @@ section 'management', "Application Management" do
 	topic 'rails3',             'Rails 3'
 	topic 'ps',                 'Processes'
 	topic 'postgresql-migrate', 'Database migration'
+	topic 'logging',            'Logging'
 end
 
 section 'features', "Platform Features" do
