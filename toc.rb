37804aafeff446ab35a83e8dc8e5c3473c5c651d
Josh Huckabee <joshhuckabee@gmail.com>
2011-04-09 11:47:54 -0700

    First draft of default prefixes documentation.

--- toc.rb
+++ toc.rb
@@ -14,6 +14,7 @@ section 'using-dydra',     "Using Dydra" do
   topic 'overview',        "Overview"
   topic 'cli',             "Command-Line Interface"
   topic 'api/sparql',      "SPARQL Endpoints"
+  topic 'prefixes',        "Prefixes"
   topic 'api/rest',        "REST API"
   topic 'api/sesame',      "Sesame API"
 end
