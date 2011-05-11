5addcd7bc2eb32cd36a5683f8d94b5828545055b
Josh Huckabee <joshhuckabee@gmail.com>
2011-05-11 11:47:38 -0700

    Add separate authentication doc and reference that from the SPARQL, REST, and Sesame API docs.

--- toc.rb
+++ toc.rb
@@ -1,24 +1,25 @@
-section 'getting-started', "Getting Started" do
-  topic 'dydra',           "What is Dydra?"
-  topic 'quickstart',      "Quickstart Guide"
-  topic 'philosophy',      "Our Philosophy"
-  topic 'faq',             "Frequently Asked Questions"
+section 'getting-started',    "Getting Started" do
+  topic 'dydra',              "What is Dydra?"
+  topic 'quickstart',         "Quickstart Guide"
+  topic 'philosophy',         "Our Philosophy"
+  topic 'faq',                "Frequently Asked Questions"
 end
 
-section 'rdf',             "RDF" do
-  topic 'rdf',             "What is RDF?"
-  topic 'sparql',          "SPARQL"
+section 'rdf',                "RDF" do
+  topic 'rdf',                "What is RDF?"
+  topic 'sparql',             "SPARQL"
 end
 
-section 'using-dydra',     "Using Dydra" do
-  topic 'overview',        "Overview"
-  topic 'cli',             "Command-Line Interface"
+section 'using-dydra',        "Using Dydra" do
+  topic 'overview',           "Overview"
+  topic 'cli',                "Command-Line Interface"
   topic 'namespace-prefixes', "Namespace Prefixes"
-  topic 'api/sparql',      "SPARQL Endpoints"
-  topic 'api/rest',        "REST API"
-  #topic 'api/sesame',      "Sesame API"
+  topic 'api/sparql',         "SPARQL Endpoints"
+  topic 'api/rest',           "REST API"
+  #topic 'api/sesame',         "Sesame API"
+  topic 'api/authentication', "API Authentication"
 end
 
-section 'sdk',             "Libraries" do
-  topic 'sdk/ruby',        "SDK for Ruby"
+section 'sdk',                "Libraries" do
+  topic 'sdk/ruby',           "SDK for Ruby"
 end
